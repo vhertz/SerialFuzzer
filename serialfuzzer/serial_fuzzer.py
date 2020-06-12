72ceda5305ed80bf72160dab6d5460e6a11b2221
vhertz <v3hertz+github@gmail.com>
2020-06-12 13:22:07 +0900

    Add a fuzz() method

--- serialfuzzer/serial_fuzzer.py
+++ serialfuzzer/serial_fuzzer.py
@@ -1,2 +1,12 @@
+import serial
+import os
 class SerialFuzzer():
-    pass
+
+    def __init__(self, serial, size):
+        self._serial = serial
+        self._size = size
+
+    def fuzz(self):
+        input = os.getrandom(self._size)
+        self._serial.write(input)
+        return
