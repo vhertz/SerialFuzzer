881e073ef80e58d894d74e8f04a179c3f498e0f1
vhertz <v3hertz+github@gmail.com>
2020-06-12 13:24:07 +0900

    Add a test

---
+++ test/test_serial_fuzzer.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+import serialfuzzer
+import serial
+import os
+
+class TestSerialFuzzer(TestCase):
+
+    def setUp(self):
+        self.master, self.slave = os.openpty()
+
+    def test_fuzz(self):
+        with serial.Serial(os.ttyname(self.slave)) as se:
+            with os.fdopen(self.master, "rb") as fd:
+                fuzzer = serialfuzzer.SerialFuzzer(se, 1)
+                input = fuzzer.fuzz()
+                se.flush()
+                output = fd.read(len(input))
+                self.assertEqual(input, output)
