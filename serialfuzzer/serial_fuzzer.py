1deaa73f283731020f0f954055f66d38dcdef43a
vhertz <v3hertz+github@gmail.com>
2020-06-12 22:42:33 +0900

    Add debug messages

--- serialfuzzer/serial_fuzzer.py
+++ serialfuzzer/serial_fuzzer.py
@@ -17,12 +17,15 @@ class SerialFuzzer():
 
     def fuzz(self):
         input = self.__gen_input()
-        logger.debug("Generated input: " + str(input))
         self.__transmit(input)
         return input
 
     def __gen_input(self):
-        return bytes(random.getrandbits(8) for _ in range(self._size))
+        logger.debug("Generate input")
+        input = bytes(random.getrandbits(8) for _ in range(self._size))
+        logger.info(input)
+        return input
 
     def __transmit(self, input):
+        logger.debug("Transmit input")
         self._serial.write(input)
